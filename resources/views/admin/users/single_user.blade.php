961d0db01e2ea37073d126ef8705e087948f6304
stani1940 <stanislav1940@abv.bg>
2020-11-08 12:09:05 +0200

    Add_logic

--- resources/views/admin/users/single_user.blade.php
+++ resources/views/admin/users/single_user.blade.php
@@ -2,57 +2,40 @@
 
 @section('content')
     <div class="content-wrapper">
-        <div class="content" id="posts">
+        <div class="content" id="user">
             <div class="container">
 
                 <div class="col-12 text-center post-title-wrap pb-3">
                     <h1 itemprop="headline" class="entry-title">{{$user->name}}</h1>
                 </div>
-                {{-- Ratings Block --}}
-
-                <div class="user_ratings">
-
-                    <form method="post" action="{{route('admin.add.rating',$user->id)}}">
-                        @csrf
-
-                        <input type="hidden" name="user_id" value="{{$user->id}}">
-                        <h2> Rating Technique</h2>
-                        <div id="rating_technique" class="rating_submit_inner">
-                            @for($i=10;$i>0;$i--)
-
-                                <input id="radio{{$i+9}}" class="star" name="rating_technique" type="radio" value="{{$i}} "
-                                       />
-                                <label for="radio{{$i+9}}">&#9733;</label>
-                            @endfor
-                        </div>
-
-                        <h2> Rating Performance</h2>
-                        <div id="rating_performance" class="rating_submit_inner">
-                            @for($i=10;$i>0;$i--)
-
-                                <input id="radio{{$i}}" name="rating_performance" type="radio" value="{{$i}}"
-                                       class="star"/>
-                                <label for="radio{{$i}}">&#9733;</label>
-                            @endfor
-                        </div>
-
-                        <h2> Rating Artistry</h2>
-                        <div id="rating_artistry" class="rating_submit_inner">
-                            @for($i=10;$i>0;$i--)
-
-                                <input id="radio{{$i+20}}" class="star" name="rating_artistry" type="radio" value="{{$i}}"
-                                       />
-                                <label for="radio{{$i+20}}">&#9733;</label>
-                            @endfor
-                        </div>
-
-                        <input  style="width: 100%" type="submit" name="submit" value="RATE" class="btn-success" id="rate">
-
-                    </form>
-
-                </div>
+                <div class="card-body">
+                    <table class="table table-dark  table-striped">
+                        <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Arbiter`s name</th>
+                            <th>Techniques`s rating</th>
+                            <th>Performance` rating</th>
+                            <th>Artistry`s rating</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @php
+                            $num = 1;
+                        @endphp
+                        @foreach($ratings as $rating)
+                            <tr>
+                                <td> {{$num++}} </td>
+                                <td> {{$rating->arbier_id}} </td>
+                                <td>{{$rating->rating_count}}</td>
+                                <td>{{$rating->rating_performance}}</td>
+                                <td>{{$rating->rating_artistry}}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
             </div>
-
         </div>
+    </div>
     </div>
 @endsection
