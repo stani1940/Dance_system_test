6f468e3801fa5a321da52a184ed029c1f0519517
stani1940 <stanislav1940@abv.bg>
2020-11-10 13:57:47 +0200

    Change destination ProfilesController

--- app/Http/Controllers/Admin/ProfilesController.php
+++ app/Http/Controllers/Admin/ProfilesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Profile;
 use Illuminate\Http\Request;
 
 class ProfilesController extends Controller
@@ -54,10 +55,16 @@ class ProfilesController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+
+
+        public function edit( Profile $profile )
     {
-        //
+        $profile = Profile::find( $profile )->first();
+        $user_data = $profile->user;
+
+        return view('profiles.edit', compact( 'profile', 'user_data' ) );
     }
+
 
     /**
      * Update the specified resource in storage.
