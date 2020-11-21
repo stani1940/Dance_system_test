d0c011770287170f33d26d868c01979c7fa167c4
stani1940 <stanislav1940@abv.bg>
2020-11-21 14:41:54 +0200

    Fix arbiter panel

--- resources/views/layouts/includes/menu.blade.php
+++ resources/views/layouts/includes/menu.blade.php
@@ -19,7 +19,7 @@
                                 <li>
                                     <a href="{{ route('login') }}">Login</a>
                                 </li>
-                               
+
 
                             @endauth
 
